d7e616f14a551829fe1312f1b516674b253ec673
Dan G <ddgroleau@gmail.com>
2021-01-26 14:19:23 -0700

    add functionality to new DOM elements

--- routes/current-user.js
+++ routes/current-user.js
@@ -7,4 +7,12 @@ const currentUser = require("../controllers/current-user-controller")
 
 Router.get("/currentUser", currentUser.getCurrentUser);
 
+Router.get("/currentUser:requests", async (request, response) => {
+    if (request.session.loggedin) {
+ response.render('user-requests.ejs')
+    } else {
+        response.render('login.ejs', {message:'Please login.'});
+    }
+});
+
 module.exports = Router
